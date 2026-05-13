6da396cb1e570e3ddda8af83ccb937db3ba4158a
dreezy-6 <ogbj316@gmail.com>
2026-05-13 03:32:52 -0500

    Add production media generation provider adapters

--- services/media_generation/__init__.py
+++ services/media_generation/__init__.py
@@ -1,12 +1,26 @@
 """Media generation services for WF-005 orchestration."""
 
-from .adapters import MediaGenerationAdapter, ProviderGenerationResult, StubGenAudioAdapter
+from .adapters import (
+    MediaGenerationAdapter,
+    ProviderGenerationResult,
+    ReplicateAudioAdapter,
+    StubGenAudioAdapter,
+    SunoAdapter,
+    UdioAdapter,
+    build_media_generation_adapter,
+    build_media_generation_adapter_from_scheduler,
+)
 from .service import ReplayContract, generate_music_for_wf005
 
 __all__ = [
     "MediaGenerationAdapter",
     "ProviderGenerationResult",
     "ReplayContract",
+    "ReplicateAudioAdapter",
     "StubGenAudioAdapter",
+    "SunoAdapter",
+    "UdioAdapter",
+    "build_media_generation_adapter",
+    "build_media_generation_adapter_from_scheduler",
     "generate_music_for_wf005",
 ]
