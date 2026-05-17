78a8bf7a7850477d1359e4f42c5ddd9afd30afd3
Dustin L. Reid <dustinreid82@gmail.com>
2026-05-17 11:33:22 -0500

    Add configurable API dispatcher with retries telemetry and idempotency

---
+++ services/integration/__init__.py
@@ -0,0 +1,15 @@
+from .api_dispatcher import (
+    ApiDispatcher,
+    DispatchError,
+    DispatchErrorEnvelope,
+    DispatchRequestEnvelope,
+    DispatchResponseEnvelope,
+)
+
+__all__ = [
+    "ApiDispatcher",
+    "DispatchError",
+    "DispatchErrorEnvelope",
+    "DispatchRequestEnvelope",
+    "DispatchResponseEnvelope",
+]
